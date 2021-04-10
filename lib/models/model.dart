6b89cdb9d4042af6f2174b40271f148dc6fcb9bb
shreyas gupta <shreyas.charming@gmail.com>
2021-04-10 10:40:21 +0530

    added family member model

---
+++ lib/models/model.dart
@@ -0,0 +1,66 @@
+class FamilyMember {
+  String id;
+  String name;
+  String relation;
+  String age;
+  String gender;
+  bool isSurveyFilled = false;
+  var surveyQuestionResponses = new Map();
+  var qualitativeStudyResponses = new Map();
+  double totalScreenTime = 0.0;
+  var hourlyScreenTimeBreakdown = [];
+
+  FamilyMember({
+    this.id,
+    this.name,
+    this.relation,
+    this.age,
+    this.gender,
+    isSurveyFilled = false,
+    totalScreenTime = 0.0,
+  });
+
+  FamilyMember.withOtherData(
+      String id,
+      String name,
+      String relation,
+      String age,
+      String gender,
+      bool isSurveyFilled,
+      Map<String, String> surveyQuestionResponses,
+      Map<String, String> qualitativeStudyResponses,
+      double totalScreenTime,
+      var hourlyScreenTimeBreakdown) {
+    this.name = name;
+    this.gender = gender;
+    this.relation = relation;
+    this.id = id;
+    this.age = age;
+    this.isSurveyFilled = isSurveyFilled;
+    this.surveyQuestionResponses = surveyQuestionResponses;
+    this.qualitativeStudyResponses = qualitativeStudyResponses;
+    this.totalScreenTime = totalScreenTime;
+    this.hourlyScreenTimeBreakdown = hourlyScreenTimeBreakdown;
+  }
+  Map toJSON() {
+    return {
+      'name': name ?? '',
+      'relation': relation ?? '',
+      'age': age ?? '',
+      'id': id ?? '',
+      'gender': gender ?? '',
+      'isSurveyFilled': isSurveyFilled,
+      'surveyQuestionResponses': surveyQuestionResponses,
+      'qualitativeStudyResponses': qualitativeStudyResponses,
+      'totalScreenTime': totalScreenTime,
+      'hourlyScreenTimeBreakdown': hourlyScreenTimeBreakdown,
+    };
+  }
+
+  FamilyMember.fromJSON(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    relation = json['relation'];
+    age = json['age'];
+  }
+}
