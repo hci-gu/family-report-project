6cec404cc49400f78ad1659dcd6143a839298e6f
shreyas gupta <shreyas.charming@gmail.com>
2021-04-16 10:58:06 +0530

    modified main dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:family_report_project/models/model.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import './services/api.dart';
 import 'package:provider/provider.dart';
-import './onboarding.dart';
-import './providers/familyMemberProvider.dart';
-import './testFamily.dart';
+import 'providers/familyMemberProvider.dart';
+import './screens/wrapper.dart';
+import 'services/api.dart';
+import './services/auth.dart';
+import './models/model.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+// import 'testing/testFamily.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,18 +20,11 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final firestoreService = Api('family1');
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-            create: (context) => FamilyMemberProvider('family1')),
-        StreamProvider(
-          create: (context) => firestoreService.getFamilyMembersList(),
-        ),
-      ],
+    // final firestoreService = Api('family1');
+    return StreamProvider<FamilyMember>.value(
+      value: AuthService().familyMemberUser,
       child: MaterialApp(
-        title: "Family Report App",
-        home: Onboarding(),
+        home: Wrapper(),
         theme: ThemeData(
           primaryColor: const Color(0xff2B9797),
           accentColor: const Color(0xffD2E5E5),
@@ -43,17 +41,26 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// return MaterialApp(
-//       title: "Family Report App",
-//       home: Onboarding(),
-//       theme: ThemeData(
-//         primaryColor: const Color(0xff2B9797),
-//         accentColor: const Color(0xffD2E5E5),
-//         textTheme: TextTheme(
-//           headline1: TextStyle(
-//             fontSize: 36,
-//             fontWeight: FontWeight.bold,
-//             color: const Color(0xff2B9797),
+// MultiProvider(
+//       providers: [
+//         ChangeNotifierProvider(
+//             create: (context) => FamilyMemberProvider('family1')),
+//         StreamProvider(
+//           create: (context) => firestoreService.getFamilyMembersList(),
+//         ),
+//       ],
+//       child: MaterialApp(
+//         title: "Family Report App",
+//         home: Wrapper(),
+//         theme: ThemeData(
+//           primaryColor: const Color(0xff2B9797),
+//           accentColor: const Color(0xffD2E5E5),
+//           textTheme: TextTheme(
+//             headline1: TextStyle(
+//               fontSize: 36,
+//               fontWeight: FontWeight.bold,
+//               color: const Color(0xff2B9797),
+//             ),
 //           ),
 //         ),
 //       ),
