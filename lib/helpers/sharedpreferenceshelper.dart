e85e6e51616554840be875ca54799e18bce27fd1
shreyas gupta <shreyas.charming@gmail.com>
2021-04-19 09:27:01 +0530

    added uid storage functions

--- lib/helpers/sharedpreferenceshelper.dart
+++ lib/helpers/sharedpreferenceshelper.dart
@@ -2,10 +2,21 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesHelper {
   static final String _collectionNameCode = "familyID";
+  static final String _familyMemberUidCode = "familyMemberUID";
 
   static Future<String> getFamilyCollectionName() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.getString(_collectionNameCode) ?? "";
+  }
+
+  static Future<String> getFamilyMemberUid() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_familyMemberUidCode) ?? "";
+  }
+
+  static Future<bool> setFamilyMemberUid(String value) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setString(_familyMemberUidCode, value);
   }
 
   static Future<bool> setFamilyCollectionName(String value) async {
