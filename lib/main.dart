921a2a4b3d0b1c453b8b92de3b0458ca8677d854
shreyas gupta <shreyas.charming@gmail.com>
2021-04-19 09:27:16 +0530

    removed dummy data

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // final firestoreService = Api('family1');
     return StreamProvider<FamilyMember>.value(
       value: AuthService().familyMemberUser,
       initialData: null,
@@ -36,28 +35,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// MultiProvider(
-//       providers: [
-//         ChangeNotifierProvider(
-//             create: (context) => FamilyMemberProvider('family1')),
-//         StreamProvider(
-//           create: (context) => firestoreService.getFamilyMembersList(),
-//         ),
-//       ],
-//       child: MaterialApp(
-//         title: "Family Report App",
-//         home: Wrapper(),
-//         theme: ThemeData(
-//           primaryColor: const Color(0xff2B9797),
-//           accentColor: const Color(0xffD2E5E5),
-//           textTheme: TextTheme(
-//             headline1: TextStyle(
-//               fontSize: 36,
-//               fontWeight: FontWeight.bold,
-//               color: const Color(0xff2B9797),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
