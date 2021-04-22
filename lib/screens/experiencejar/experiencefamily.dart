3f3b88df4d8b12324e9f8e7108cdc62406f26a29
shreyas gupta <shreyas.charming@gmail.com>
2021-04-22 22:07:23 +0530

    fixed keyboard hiding form field

--- lib/screens/experiencejar/experiencefamily.dart
+++ lib/screens/experiencejar/experiencefamily.dart
@@ -13,57 +13,68 @@ class ExperienceFamily extends StatefulWidget {
 }
 
 class _ExperienceFamilyState extends State<ExperienceFamily> {
-  final _formKey = GlobalKey<FormState>();
   String familyInput = "";
   @override
   Widget build(BuildContext context) {
+    final bottom = MediaQuery.of(context).viewInsets.bottom;
+    final node = FocusScope.of(context);
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    return Container(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Experience Jar - family"),
-        ),
-        body: Container(
-          margin: EdgeInsets.fromLTRB(width / 30, 10, width / 30, 30),
-          child: Column(
-            children: [
-              Text(
-                "1. Tell us about at least one significant observation you made about your own smartphone usage",
-                style: TextStyle(
-                  fontSize: height / 30,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Experience Jar - family"),
+      ),
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Padding(
+          padding: EdgeInsets.only(bottom: bottom),
+          child: Container(
+            margin: EdgeInsets.fromLTRB(width / 30, 10, width / 30, 30),
+            child: Column(
+              children: [
+                Text(
+                  "1. Tell us about at least one significant observation you made about your own smartphone usage",
+                  style: TextStyle(
+                    fontSize: height / 30,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                minLines: 5,
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter your response to the question',
+                SizedBox(
+                  height: 20,
                 ),
-                validator: (val) =>
-                    val.isEmpty ? 'Please enter your response.' : null,
-                onChanged: (val) {
-                  setState(() {
-                    familyInput = val;
-                  });
-                },
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Spacer(),
-              RegularGreenButton("Finish Daily Log", () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomeScreen()),
-                );
-              }),
-            ],
+                TextFormField(
+                  textInputAction: TextInputAction.done,
+                  onEditingComplete: () => node.nextFocus(),
+                  minLines: 5,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your response to the question',
+                  ),
+                  validator: (val) =>
+                      val.isEmpty ? 'Please enter your response.' : null,
+                  onChanged: (val) {
+                    setState(() {
+                      familyInput = val;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Spacer(),
+                RegularGreenButton("Finish Daily Log", () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => HomeScreen(
+                              familyId: widget.familyId,
+                              loggedInUserUid: widget.currentLoggedInUserUid,
+                            )),
+                  );
+                }),
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/experiencejar/experienceself.dart
+++ lib/screens/experiencejar/experienceself.dart
@@ -18,6 +18,8 @@ class _ExperienceSelfState extends State<ExperienceSelf> {
 
   @override
   Widget build(BuildContext context) {
+    final bottom = MediaQuery.of(context).viewInsets.bottom;
+    final node = FocusScope.of(context);
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Container(
@@ -25,62 +27,67 @@ class _ExperienceSelfState extends State<ExperienceSelf> {
         appBar: AppBar(
           title: Text("Experience Jar - Self"),
         ),
-        body: Column(
-          children: [
-            Form(
-              key: _formKey,
-              child: Container(
-                margin: EdgeInsets.fromLTRB(width / 30, 10, width / 30, 30),
-                child: Column(
-                  children: [
-                    Text(
-                      "1. Tell us about at least one significant observation you made about your own smartphone usage",
-                      style: TextStyle(
-                        fontSize: height / 30,
+        body: SingleChildScrollView(
+          reverse: true,
+          child: Padding(
+            padding: EdgeInsets.only(bottom: bottom),
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(width / 30, 10, width / 30, 30),
+                  child: Column(
+                    children: [
+                      Text(
+                        "1. Tell us about at least one significant observation you made about your own smartphone usage",
+                        style: TextStyle(
+                          fontSize: height / 30,
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      keyboardType: TextInputType.multiline,
-                      maxLines: null,
-                      minLines: 5,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Enter your response to the question',
+                      SizedBox(
+                        height: 20,
                       ),
-                      validator: (val) =>
-                          val.isEmpty ? 'Please enter your response.' : null,
-                      onChanged: (val) {
-                        setState(() {
-                          selfInput = val;
-                        });
-                      },
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                  ],
+                      TextFormField(
+                        textInputAction: TextInputAction.done,
+                        onEditingComplete: () => node.nextFocus(),
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                        minLines: 5,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Enter your response to the question',
+                        ),
+                        validator: (val) =>
+                            val.isEmpty ? 'Please enter your response.' : null,
+                        onChanged: (val) {
+                          setState(() {
+                            selfInput = val;
+                          });
+                        },
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+                Spacer(),
+                Container(
+                  margin: EdgeInsets.fromLTRB(width / 30, 10, width / 30, 30),
+                  child: RegularGreenButton("Continue", () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ExperienceFamily(
+                                familyId: widget.familyId,
+                                currentLoggedInUserUid:
+                                    widget.currentLoggedInUserUid,
+                              )),
+                    );
+                  }),
+                )
+              ],
             ),
-            Spacer(),
-            Container(
-              margin: EdgeInsets.fromLTRB(width / 30, 10, width / 30, 30),
-              child: RegularGreenButton("Continue", () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => ExperienceFamily(
-                            familyId: widget.familyId,
-                            currentLoggedInUserUid:
-                                widget.currentLoggedInUserUid,
-                          )),
-                );
-              }),
-            )
-          ],
+          ),
         ),
       ),
     );
