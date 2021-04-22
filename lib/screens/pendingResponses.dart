f47d3e453be1ef282b00cfef8c2aeedaca4e65e2
shreyas gupta <shreyas.charming@gmail.com>
2021-04-22 18:21:14 +0530

    remove component on survey completion

--- lib/screens/pendingResponses.dart
+++ lib/screens/pendingResponses.dart
@@ -15,22 +15,22 @@ class _FamilyMemberListState extends State<PendingResponses> {
     double height = MediaQuery.of(context).size.height;
     var familyMemberList = Provider.of<List<FamilyMember>>(context) ?? [];
     int pendingResponses = 0;
-    print("widget rebuilt");
     for (var family in familyMemberList) {
       if (family.isSurveyFilled == false) {
-        print(family.isSurveyFilled);
         pendingResponses = pendingResponses + 1;
       }
     }
 
-    return Text(
-      "$pendingResponses Pending Responses",
-      overflow: TextOverflow.fade,
-      maxLines: 1,
-      style: TextStyle(
-        fontSize: height / 50,
-        color: Theme.of(context).primaryColor,
-      ),
-    );
+    return (pendingResponses == 0)
+        ? Container()
+        : Text(
+            "$pendingResponses Pending Responses",
+            overflow: TextOverflow.fade,
+            maxLines: 1,
+            style: TextStyle(
+              fontSize: height / 50,
+              color: Theme.of(context).primaryColor,
+            ),
+          );
   }
 }
