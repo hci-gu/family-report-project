ca7678845aead5629ae45316447b4e5a671f8268
shreyas gupta <shreyas.charming@gmail.com>
2021-04-22 18:18:13 +0530

    fixed issue where isSurveyFilled is set to false

--- lib/models/model.dart
+++ lib/models/model.dart
@@ -7,8 +7,8 @@ class FamilyMember {
   bool isSurveyFilled = false;
   var surveyQuestionResponses = new Map();
   var qualitativeStudyResponses = new Map();
-  double totalScreenTime = 0.0;
-  var hourlyScreenTimeBreakdown = [];
+  Map<String, double> totalScreenTime = new Map();
+  Map<String, List<double>> hourlyScreenTimeBreakdown = new Map();
 
   FamilyMember({
     this.id,
@@ -19,52 +19,7 @@ class FamilyMember {
     this.qualitativeStudyResponses,
     this.surveyQuestionResponses,
     this.hourlyScreenTimeBreakdown,
-    isSurveyFilled = false,
-    totalScreenTime = 0.0,
+    this.totalScreenTime,
+    this.isSurveyFilled,
   });
-
-  FamilyMember.withOtherData(
-      String id,
-      String name,
-      String relation,
-      String age,
-      String gender,
-      bool isSurveyFilled,
-      Map<String, Map<String, String>> surveyQuestionResponses,
-      Map<String, Map<String, String>> qualitativeStudyResponses,
-      double totalScreenTime,
-      var hourlyScreenTimeBreakdown) {
-    this.name = name;
-    this.gender = gender;
-    this.relation = relation;
-    this.id = id;
-    this.age = age;
-    this.isSurveyFilled = isSurveyFilled;
-    this.surveyQuestionResponses = surveyQuestionResponses;
-    this.qualitativeStudyResponses = qualitativeStudyResponses;
-    this.totalScreenTime = totalScreenTime;
-    this.hourlyScreenTimeBreakdown = hourlyScreenTimeBreakdown;
-  }
-  Map<String, dynamic> toJSON() {
-    return {
-      'name': name ?? '',
-      'relation': relation ?? '',
-      'age': age ?? '',
-      'id': id ?? '',
-      'gender': gender ?? '',
-      'isSurveyFilled': isSurveyFilled,
-      'surveyQuestionResponses': surveyQuestionResponses,
-      'qualitativeStudyResponses': qualitativeStudyResponses,
-      'totalScreenTime': totalScreenTime,
-      'hourlyScreenTimeBreakdown': hourlyScreenTimeBreakdown,
-    };
-  }
-
-  FamilyMember.fromJSON(Map<String, dynamic> json) {
-    print(json);
-    id = json['id'];
-    name = json['name'];
-    relation = json['relation'];
-    age = json['age'];
-  }
 }
