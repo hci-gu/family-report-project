333ea2d559a232148b22e22667109f989fdaceb2
shreyas gupta <shreyas.charming@gmail.com>
2021-04-23 05:23:11 +0530

    fixed keyboard covering form issue

--- lib/screens/experiencejar/experiencefamily.dart
+++ lib/screens/experiencejar/experiencefamily.dart
@@ -21,8 +21,16 @@ class _ExperienceFamilyState extends State<ExperienceFamily> {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
-        title: Text("Experience Jar - family"),
+        iconTheme: IconThemeData(
+          color: Theme.of(context).primaryColor,
+        ),
+        elevation: 0,
+        title: Text("Experience Jar - Family",
+            style: TextStyle(color: Theme.of(context).primaryColor)),
+        backgroundColor: Colors.white,
+        foregroundColor: Theme.of(context).primaryColor,
       ),
       body: SingleChildScrollView(
         reverse: true,
@@ -62,7 +70,6 @@ class _ExperienceFamilyState extends State<ExperienceFamily> {
                 SizedBox(
                   height: 20,
                 ),
-                Spacer(),
                 RegularGreenButton("Finish Daily Log", () {
                   Navigator.push(
                     context,
--- lib/screens/experiencejar/experienceself.dart
+++ lib/screens/experiencejar/experienceself.dart
@@ -22,68 +22,66 @@ class _ExperienceSelfState extends State<ExperienceSelf> {
     final node = FocusScope.of(context);
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    return Container(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Experience Jar - Self"),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Theme.of(context).primaryColor,
         ),
-        body: SingleChildScrollView(
-          reverse: true,
-          child: Padding(
-            padding: EdgeInsets.only(bottom: bottom),
+        elevation: 0,
+        title: Text("Experience Jar - Self",
+            style: TextStyle(color: Theme.of(context).primaryColor)),
+        backgroundColor: Colors.white,
+        foregroundColor: Theme.of(context).primaryColor,
+      ),
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Padding(
+          padding: EdgeInsets.only(bottom: bottom),
+          child: Container(
+            margin: EdgeInsets.fromLTRB(width / 30, 10, width / 30, 30),
             child: Column(
               children: [
-                Container(
-                  margin: EdgeInsets.fromLTRB(width / 30, 10, width / 30, 30),
-                  child: Column(
-                    children: [
-                      Text(
-                        "1. Tell us about at least one significant observation you made about your own smartphone usage",
-                        style: TextStyle(
-                          fontSize: height / 30,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        textInputAction: TextInputAction.done,
-                        onEditingComplete: () => node.nextFocus(),
-                        keyboardType: TextInputType.multiline,
-                        maxLines: null,
-                        minLines: 5,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Enter your response to the question',
-                        ),
-                        validator: (val) =>
-                            val.isEmpty ? 'Please enter your response.' : null,
-                        onChanged: (val) {
-                          setState(() {
-                            selfInput = val;
-                          });
-                        },
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                    ],
+                Text(
+                  "1. Tell us about at least one significant observation you made about your own smartphone usage",
+                  style: TextStyle(
+                    fontSize: height / 30,
                   ),
                 ),
-                Spacer(),
-                Container(
-                  margin: EdgeInsets.fromLTRB(width / 30, 10, width / 30, 30),
-                  child: RegularGreenButton("Continue", () {
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  textInputAction: TextInputAction.done,
+                  onEditingComplete: () => node.nextFocus(),
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  minLines: 5,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your response to the question',
+                  ),
+                  validator: (val) =>
+                      val.isEmpty ? 'Please enter your response.' : null,
+                  onChanged: (val) {
+                    setState(() {
+                      selfInput = val;
+                    });
+                  },
+                ),
+                RegularGreenButton(
+                  "Continue",
+                  () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => ExperienceFamily(
-                                familyId: widget.familyId,
-                                currentLoggedInUserUid:
-                                    widget.currentLoggedInUserUid,
-                              )),
+                        builder: (context) => ExperienceFamily(
+                          familyId: widget.familyId,
+                          currentLoggedInUserUid: widget.currentLoggedInUserUid,
+                        ),
+                      ),
                     );
-                  }),
+                  },
                 )
               ],
             ),
