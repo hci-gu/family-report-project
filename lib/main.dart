412fde1cfefb27ae5e624e847e3df39b1b2ec226
shreyas gupta <shreyas.charming@gmail.com>
2021-05-04 21:42:26 +0530

    added notifications support

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:family_report_project/models/model.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import './screens/wrapper.dart';
 import './services/auth.dart';
 import './models/model.dart';
+import './helpers/notificationhelpers.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+NotificationAppLaunchDetails notificationAppLaunchDetails;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  notificationAppLaunchDetails =
+      await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+  await initNotifications(flutterLocalNotificationsPlugin);
+  requestIOSPermissions(flutterLocalNotificationsPlugin);
+
   runApp(MyApp());
 }
 
